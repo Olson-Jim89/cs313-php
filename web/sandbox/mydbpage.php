a71c7593c26e1a3b0fe2ad002b30532c98a4596a
jimolson89 <jim.olson1989@gmail.com>
2018-02-05 10:15:18 -0700

    update 2

--- web/sandbox/mydbpage.php
+++ web/sandbox/mydbpage.php
@@ -17,6 +17,13 @@
 				echo 'Error!: ' . $ex->getMessage();
 				die();
 			}
+			
+foreach ($db->query('SELECT username, password FROM note_user') as $row)
+{
+  echo 'user: ' . $row['username'];
+  echo ' password: ' . $row['password'];
+  echo '<br/>';
+}
 		?>
 	</body>
 </html>
