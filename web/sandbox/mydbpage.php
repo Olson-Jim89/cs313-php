9ad6a379a248d62c53f2506999e0d931f1744b48
jimolson89 <jim.olson1989@gmail.com>
2018-02-05 10:43:06 -0700

    sandbox update 8

--- web/sandbox/mydbpage.php
+++ web/sandbox/mydbpage.php
@@ -19,7 +19,7 @@
 				echo 'Error!: ' . $ex->getMessage();
 				die();
 			}
-		
+/*			
 foreach ($db->query('SELECT username, password FROM note_user') as $row)
 {
   echo 'user: ' . $row['username'];
@@ -33,10 +33,10 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 {
   echo 'user: ' . $row['username'] . ' password: ' . $row['password'] . '<br/>';
 }
-/*	
+
 $statement = $db->query('SELECT username, password FROM note_user');
 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-
+*/
 
 $stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
 $stmt->bindValue(':id', $id, PDO::PARAM_INT);
@@ -44,7 +44,7 @@ $stmt->bindValue(':name', $name, PDO::PARAM_STR);
 $stmt->execute();
 $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 echo 'made it here':
-*/
+
 		?>
 	</body>
 </html>
