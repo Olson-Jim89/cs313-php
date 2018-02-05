b03ce860922c40bf253d7aeaa3ccbdadfb74ae3d
jimolson89 <jim.olson1989@gmail.com>
2018-02-05 10:32:33 -0700

    update 3

--- web/sandbox/mydbpage.php
+++ web/sandbox/mydbpage.php
@@ -29,6 +29,12 @@ foreach ($db->query('SELECT username, password FROM note_user') as $row)
 
 
 $statement = $db->query('SELECT username, password FROM note_user');
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+  echo 'user: ' . $row['username'] . ' password: ' . $row['password'] . '<br/>';
+}
+
+$statement = $db->query('SELECT username, password FROM note_user');
 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 		?>
