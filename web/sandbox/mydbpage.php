050c83845a68cfdf1eb2cd39dcc98b7f8094b3ee
jimolson89 <jim.olson1989@gmail.com>
2018-02-05 11:32:00 -0700

    sandbox update 10

--- web/sandbox/mydbpage.php
+++ web/sandbox/mydbpage.php
@@ -56,7 +56,7 @@ foreach ($db->query('SELECT now()') as $row)
 				die();
 			}
 			*/
-/*
+
 foreach ($db->query('SELECT username, password FROM note_user') as $row)
 {
   echo 'user: ' . $row['username'];
@@ -71,9 +71,7 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
   echo 'user: ' . $row['username'] . ' password: ' . $row['password'] . '<br/>';
 }
 
-$statement = $db->query('SELECT username, password FROM note_user');
-$results = $statement->fetchAll(PDO::FETCH_ASSOC);
-*/
+
 /*
 $stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
 $stmt->bindValue(':id', $id, PDO::PARAM_INT);
