4ab5a4d209c61853673df2ecc1b3057e851f2ae1
jimolson89 <jim.olson1989@gmail.com>
2018-02-05 11:17:08 -0700

    sandbox update 9

--- web/sandbox/mydbpage.php
+++ web/sandbox/mydbpage.php
@@ -6,20 +6,57 @@
 	<body>
 		<h1>test</h1>
 		<?php
+			// default Heroku Postgres configuration URL
+$dbUrl = getenv('DATABASE_URL');
+
+if (empty($dbUrl)) {
+ // example localhost configuration URL with postgres username and a database called cs313db
+ $dbUrl = "postgres://postgres:password@localhost:5432/cs313db";
+}
+
+$dbopts = parse_url($dbUrl);
+
+print "<p>$dbUrl</p>\n\n";
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+print "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName</p>\n\n";
+
+try {
+ $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+	echo 'works';
+}
+catch (PDOException $ex) {
+ print "<p>error: $ex->getMessage() </p>\n\n";
+ die();
+}
+
+foreach ($db->query('SELECT now()') as $row)
+{
+ print "<p>$row[0]</p>\n\n";
+}
+
+
+/*
 			try
 			{
 				$user = 'postgres';
 				$password = 'SkyGate4567!';
 				$db = new PDO('pgsql:host=127.0.0.1;dbname=notes', $user, $password);
 				echo 'Connection Established...';
-				echo '<br/>';
+				echo '<br/><br/>';
 			}
 			catch (PDOException $ex)
 			{
 				echo 'Error!: ' . $ex->getMessage();
 				die();
 			}
-/*			
+			*/
+/*
 foreach ($db->query('SELECT username, password FROM note_user') as $row)
 {
   echo 'user: ' . $row['username'];
@@ -37,13 +74,14 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 $statement = $db->query('SELECT username, password FROM note_user');
 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 */
-
+/*
 $stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
 $stmt->bindValue(':id', $id, PDO::PARAM_INT);
 $stmt->bindValue(':name', $name, PDO::PARAM_STR);
 $stmt->execute();
 $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 echo 'made it here':
+*/
 
 		?>
 	</body>
