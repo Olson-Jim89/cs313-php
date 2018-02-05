6ea20a8c900c0a95dca2b068bff84b47da544758
jimolson89 <jim.olson1989@gmail.com>
2018-02-05 11:44:57 -0700

    sandbox update 11

--- web/sandbox/mydbpage.php
+++ web/sandbox/mydbpage.php
@@ -11,7 +11,7 @@ $dbUrl = getenv('DATABASE_URL');
 
 if (empty($dbUrl)) {
  // example localhost configuration URL with postgres username and a database called cs313db
- $dbUrl = "postgres://postgres:password@localhost:5432/cs313db";
+ $dbUrl = "postgres://postgres:password@localhost:5432/notes";
 }
 
 $dbopts = parse_url($dbUrl);
